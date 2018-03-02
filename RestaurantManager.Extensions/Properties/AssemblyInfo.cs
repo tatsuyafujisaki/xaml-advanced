68ea77a4de9f3bec169a8285b8390a5ee1a42149
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-03-03 01:43:48 +0900

    Update platforms

--- RestaurantManager.Extensions/Properties/AssemblyInfo.cs
+++ RestaurantManager.Extensions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RestaurantManager.Extensions")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- RestaurantManager.Models/Properties/AssemblyInfo.cs
+++ RestaurantManager.Models/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RestaurantManager.Models")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
